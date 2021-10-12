c6ef4864910c77120e1444080473b05108c66a73
Adam Kjær Nielsen <6605393@RTS-365.dk>
2021-10-12 14:09:22 +0200

    added several things

--- src/App.js
+++ src/App.js
@@ -1,9 +1,109 @@
-import './App.css';
+import './output.css';
+
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="container">
+            <div className="container__scroll">
+                <h1 className="container__scroll--header">Mr. Jones</h1>
+                <h2 className="container__scroll--subheader">"Mr. Jones is a sadistic, 
+                        degenerate fish-like humanoid 
+                        that once inhabited the shores
+                         and islands of the surface world."</h2>
+
+                
+                <p className="container__scroll--quote"></p>
+                <img className="container__scroll--bat" src="images/bat.png" alt="ak47"></img>
+                <div className="container__scroll__extreme">
+                    <h2 className="container__scroll__extreme--subheader2">
+                        True Danish Euro Dance
+                    </h2>
+                </div>
+            </div>
+                <img className="wrmf" src="images/wrmf_true.png" alt="le_fishy"></img>
+                
+            <div className="bird-container bird-container--one">
+                <div className="bird bird--one"></div>
+            </div>
+            
+            <div className="bird-container bird-container--two">
+                <div className="bird bird--two"></div>
+            </div>
+            
+            <div className="bird-container bird-container--three">
+                <div className="bird bird--three"></div>
+            </div>
+            
+            <div className="bird-container bird-container--four">
+                <div className="bird bird--four"></div>
+            </div>
+
+            <div className="bird-container bird-container--five">
+                <div className="bird bird--five"></div>
+            </div>
+
+            <div className="bird-container bird-container--six">
+                <div className="bird bird--six"></div>
+            </div>
+
+            <div className="bird-container bird-container--seven">
+                <div className="bird bird--seven"></div>
+            </div>
+
+            <div className="bird-container bird-container--eight">
+                <div className="bird bird--eight"></div>
+            </div>
+
+            <div className="bird-container bird-container--nine">
+                <div className="bird bird--nine"></div>
+            </div>
+
+            <div className="bird-container bird-container--ten">
+                <div className="bird bird--ten"></div>
+            </div>
+
+            <div className="bird-container bird-container--eleven">
+                <div className="bird bird--eleven"></div>
+            </div>
+
+            <div className="bird-container bird-container--twelve">
+                <div className="bird bird--twelve"></div>
+            </div>
+
+            <div className="bird-container bird-container--thirteen">
+                <div className="bird bird--thirteen"></div>
+            </div>
+
+            <div className="bird-container bird-container--fourteen">
+                <div className="bird bird--fourteen"></div>
+            </div>
+
+            <div className="bird-container bird-container--fifteen">
+                <div className="bird bird--fifteen"></div>
+            </div>
+
+            <div className="bird-container bird-container--sixteen">
+                <div className="bird bird--sixteen"></div>
+            </div>
+
+            <div className="bird-container bird-container--seventeen">
+                <div className="bird bird--seventeen"></div>
+            </div>
+
+            <div className="bird-container bird-container--eighteen">
+                <div className="bird bird--eighteen"></div>
+            </div>
+
+            <div className="bird-container bird-container--nineteen">
+                <div className="bird bird--nineteen"></div>
+            </div>
+
+            <div className="bird-container bird-container--twenty">
+                <div className="bird bird--twenty"></div>
+            </div>      
+            
+        </div>
     </div>
   );
 }
